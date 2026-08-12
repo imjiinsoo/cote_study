600f231b3882f6c83f911cc7e9cb2f9a3ffc2117
imjiinsoo <jsbd0109@naver.com>
2026-08-12 17:14:20 +0900

    5-9

---
+++ chapter5/5-9.py
@@ -0,0 +1,35 @@
+# BFS 예제
+from collections import deque
+
+def bfs(graph, start, visited):
+    # queue 구현을 위한 deque 라이브러리 사용
+    queue = deque([start])
+
+    # 현재 노드 방문 처리
+    visited[start] = True
+
+    # 큐가 빌 때까지 반복
+    while queue:
+        v = queue.popleft()
+        print(v, end = " ")
+        # 해당 원소와 연결된 애들 큐에 넣기
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited = [False] * 9
+
+bfs(graph,1,visited)
